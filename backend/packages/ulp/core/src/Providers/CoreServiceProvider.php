37f45098751c3528eea01576c2ab37b1f115c267
OskarP <oskar43619@wp.pl>
2025-10-19 17:03:50 +0200

    backend cms  init with som fix in core provider

--- backend/packages/ulp/core/src/Providers/CoreServiceProvider.php
+++ backend/packages/ulp/core/src/Providers/CoreServiceProvider.php
@@ -8,22 +8,22 @@ use Illuminate\Support\ServiceProvider;
 
 class CoreServiceProvider extends ServiceProvider {
 	
-  public const Core_ROOT_PATH = __DIR__ . '/../../';
+  public const CORE_ROOT_PATH = __DIR__ . '/../../';
 
 	/**
 	 * Register any application services.
 	 */
 	public function register(): void {
-		// $coreConfig = require self::Core_ROOT_PATH . 'config/';
+		// $coreConfig = require self::CORE_ROOT_PATH . 'config/';
 	}
 
 	/**
 	 * Bootstrap any application services.
 	 */
 	public function boot(): void {
-    $this->loadRoutesFrom(self::Core_ROOT_PATH . 'routes/web.php');
-    $this->loadMigrationsFrom(self::Core_ROOT_PATH . 'database/migrations');
-    $this->loadViewsFrom(self::Core_ROOT_PATH .'resources/views', 'core');
+    $this->loadRoutesFrom(self::CORE_ROOT_PATH . 'routes/web.php');
+    $this->loadMigrationsFrom(self::CORE_ROOT_PATH . 'database/migrations');
+    $this->loadViewsFrom(self::CORE_ROOT_PATH .'resources/views', 'core');
 	}
 
 }
