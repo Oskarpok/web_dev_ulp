f4299bf78186b82e6e22a42e648a049b28d4d07a
OskarP <oskar43619@wp.pl>
2025-10-21 21:13:42 +0200

    backend core params and users controler

---
+++ backend/packages/ulp/core/src/Http/Controllers/Core/ParamController.php
@@ -0,0 +1,134 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ulp\Core\Http\Controllers\Cms;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Ulp\Core\View\FormFields\Text\TextTypeController;
+use Ulp\Core\View\FormFields\Buttons\ButtonsTypeController;
+use Ulp\Core\View\FormFields\DateTime\DateTimeTypeControl;
+
+class ParamController extends \Ulp\Core\Http\Controllers\BaseController {
+
+  protected const MODEL_CLASS = \Ulp\Core\Models\Cms\Param::class;
+  protected const ROUTE_NAME = 'cms.params.';
+
+  protected function titles(): array {
+    return [
+      'index' => 'Parameters Panel',
+      'create' => 'Parameters Create Panel',
+      'edit' => 'Parameters Edit Panel',
+      'show' => 'Parameters Show Panel',
+    ];
+  }
+
+  protected function indexPrepare(Request $request): array {
+    return [
+      'data' => self::MODEL_CLASS::filter($request, [
+        'id', 'name', 'type', 'value', 'created_at', 'updated_at',
+      ])->get()->append(['type_label', 'value']), 
+      'labels' => [
+        'Id', 'Name', 'Type', 'Value', 'Created at', 'Updated at',
+      ],
+      'filterable' => [
+        'id' => true, 'name' => true, 'type' => true, 'value' => false, 
+        'created_at' => true, 'updated_at' => true,
+      ], 
+      'destinations' => self::ROUTE_NAME,
+      'buttons' => [
+        // new ButtonsTypeController([
+        //   'type' => 'anchore',
+        //   'route' => route(self::ROUTE_NAME . 'create'),
+        //   'label' => 'Dodaj parametr',
+        //   'icone' => 'fa-solid fa-plus',
+        // ]),
+      ],
+    ];
+  }
+
+  protected function prepareFormFields($data = null): array {
+    $currentRoute = Route::currentRouteName();
+    $validationRules = self::MODEL_CLASS::validationRules();
+    return [
+      // 'fields' => [
+      //   (function($currentRoute, $id) {
+      //     if($currentRoute !== self::ROUTE_NAME . 'create') {
+      //       return new TextTypeController([
+      //         'type' => 'number',
+      //         'name' => 'id',
+      //         'label' => 'ID',
+      //         'value' => $id,
+      //         'readonly' => true,
+      //       ]);
+      //     }
+      //   })($currentRoute, $data?->id),
+      //   new TextTypeController([
+      //     'type' => 'text',
+      //     'name' => 'name',
+      //     'label' => 'Nazwa',
+      //     'value' => $data?->name,
+      //     'required' => true,
+      //     'validation' => $validationRules['name'],
+      //     'readonly' => $currentRoute !== self::ROUTE_NAME . 'show' 
+      //       ? false : true,
+      //   ]),
+      //   new \Ulp\Core\View\FormFields\Select\SelectTypeControl([
+      //     'type' => 'select',
+      //     'name' => 'type',
+      //     'label' => 'Type',
+      //     'options' => \Ulp\Core\Enums\ParamsType::toArray() ?? [],
+      //     'required' => true,
+      //     'value' => $data?->type,
+      //     'readonly' => $currentRoute !== self::ROUTE_NAME . 'show' 
+      //       ? false : true,
+      //   ]),
+
+
+      //   // value
+
+      //   (function($currentRoute, $created_at) {
+      //     if($currentRoute !== self::ROUTE_NAME . 'create') {
+      //       return new DateTimeTypeControl([
+      //         'type' => 'date_time',
+      //         'name' => 'created_at',
+      //         'label' => 'Utworzony',
+      //         'readonly' => true,
+      //         'value' => $created_at,
+      //       ]);
+      //     }
+      //   })($currentRoute, $data?->created_at),
+      //   (function($currentRoute, $updated_at) {
+      //     if($currentRoute !== self::ROUTE_NAME . 'create') {
+      //       return new DateTimeTypeControl([
+      //         'type' => 'date_time',
+      //         'name' => 'updated_at',
+      //         'label' => 'Zaktualizowany',
+      //         'readonly' => true,
+      //         'value' => $updated_at,
+      //       ]);
+      //     }
+      //   })($currentRoute, $data?->updated_at),
+      // ],
+      // 'buttons' => [
+      //   (function($currentRoute) {
+      //     if ($currentRoute !== self::ROUTE_NAME . 'show') {
+      //       return new ButtonsTypeController([
+      //         'type' => 'submit',
+      //         'label' => 'Zapisz',
+      //         'icone' => 'fa-solid fa-file',
+      //       ]);
+      //     }
+      //   })($currentRoute),
+      //   new ButtonsTypeController([
+      //     'type' => 'anchore',
+      //     'route' => route(self::ROUTE_NAME . 'index'),
+      //     'label' => 'Powrut',
+      //     'icone' => 'fa-solid fa-arrow-left',
+      //   ]),
+      // ]
+    ];
+  }
+
+}
---
+++ backend/packages/ulp/core/src/Http/Controllers/Core/UserController.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ulp\Core\Http\Controllers\Cms;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Ulp\Core\View\FormFields\Buttons\ButtonsTypeController;
+
+class UserController extends \Ulp\Core\Http\Controllers\BaseController {
+
+  protected const MODEL_CLASS = \Ulp\Core\Models\Cms\User::class;
+  protected const ROUTE_NAME = 'cms.users.';
+
+  protected function titles(): array {
+    return [
+      'index' => 'Users Panel',
+      'create' => 'Users Create Panel',
+      'edit' => 'Users Edit Panel',
+      'show' => 'Users Show Panel',
+    ];
+  }
+
+  protected function indexPrepare(Request $request): array {
+    return [
+      'data' => self::MODEL_CLASS::filter($request, [
+        'id', 'first_name', 'sur_name', 'phone', 'email', 'type', 'is_active',
+        'email_verified_at', 'created_at', 'updated_at',
+      ])->get()->append(['is_active_label', 'type_label']), 
+      'labels' => [
+        'Id', 'First name', 'Sur name', 'Phone', 'Email', 'Type', 'Active',
+        'Verified at', 'Created at', 'Updated at',
+      ],
+      'filterable' => [
+        'id' => true, 'first_name' => true, 'sur_name' => true, 'phone' => true, 
+        'email' => true, 'type' => true, 'is_active' => true,
+        'email_verified_at' => true, 'created_at' => true, 'updated_at' => true,
+      ], 
+      'destinations' => self::ROUTE_NAME,
+      'buttons' => [
+        // new ButtonsTypeController([
+        //   'type' => 'anchore',
+        //   'route' => self::ROUTE_NAME . 'create',
+        //   'label' => 'Dodaj urzytkownika',
+        //   'icone' => 'fa-solid fa-plus',
+        // ]),
+      ],
+    ];
+  }
+
+  protected function prepareFormFields($data = null): array {
+    $currentRoute = Route::currentRouteName();
+    return [
+      'fields' => [
+        //
+      ],
+      'buttons' => [
+        //
+      ],
+    ];
+  }
+
+}
