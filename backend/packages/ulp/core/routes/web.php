ab279c97d349130314c5fbbf03f7eb0129bff29e
OskarP <oskar43619@wp.pl>
2025-10-21 22:26:30 +0200

    Update web.php

--- backend/packages/ulp/core/routes/web.php
+++ backend/packages/ulp/core/routes/web.php
@@ -1,3 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+Route::prefix('admin')->name('admin.')
+  ->namespace('\Ulp\Core\Http\Controllers\Core')
+  ->group(function () {
+    Route::resource('params', 'ParamController');
+    Route::resource('users', 'UserController');
+    
+});
