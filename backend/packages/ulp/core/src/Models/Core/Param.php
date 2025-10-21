466bf09f5fa7cd664ccb113d2a7e3d08e26ffd0c
OskarP <oskar43619@wp.pl>
2025-10-21 21:05:45 +0200

    backend core params and users models

---
+++ backend/packages/ulp/core/src/Models/Core/Param.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ulp\Core\Models\Cms;
+
+use Ulp\Core\Enums\ParamsType;
+
+class Param extends \Ulp\Core\Models\Base{
+  
+  protected $fillable = [
+    'name', 'type', 'val_string', 'val_int', 'val_float', 'val_bool', 'val_json',
+  ];
+
+  public static function validationRules(): array {
+    return [
+      'name' => ['required', 'string', 'max:255'],
+      'type' => ['required', 'integer'],
+      'val_string' => ['nullable'],
+      'val_int' => ['nullable'],
+      'val_float' => ['nullable'],
+      'val_bool' => ['nullable'],
+      'val_json' => ['nullable'],
+    ];
+  }
+
+  public function getTypeLabelAttribute(): string {
+    return ParamsType::tryFrom($this->type)?->name ?? ParamsType::String->name;
+  }
+ 
+  public function getValueAttribute() {
+    return match ($this->type) {
+      1 => $this->val_int,
+      2 => $this->val_float,
+      3 => $this->val_string, 
+      4 => $this->val_bool,
+      5 => $this->val_json,
+      default => null,
+    };
+  }
+
+}
---
+++ backend/packages/ulp/core/src/Models/Core/User.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ulp\Core\Models\Cms;
+
+use Ulp\Core\Enums\UsersType;
+use Ulp\Core\Traits\DefaultModel;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class User extends \Illuminate\Foundation\Auth\User {
+
+  /** @use HasFactory<\Database\Factories\UserFactory> */
+  use HasFactory, Notifiable, DefaultModel;
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var list<string>
+   */
+  protected $fillable = [
+    'first_name',
+    'is_active',
+    'sur_name',
+    'phone',
+    'email',
+  ];
+
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var list<string>
+   */
+  protected $hidden = [
+    'password',
+    'remember_token',
+  ];
+
+  public static function validationRules(): array {
+    return [
+      //
+    ];
+  }
+
+  public function getTypeLabelAttribute(): string {
+    return UsersType::tryFrom($this->type)?->name ?? UsersType::Undefined->name;
+  }
+
+  public function getIsActiveLabelAttribute(): string {
+    return $this->getAttribute('is_active') ? 'Yes' : 'No';
+  }
+
+}
