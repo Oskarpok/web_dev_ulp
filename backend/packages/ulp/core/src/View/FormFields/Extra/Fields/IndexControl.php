9dce6ce6f03f6b58510f5d9470039ebe2f51148a
OskarP <oskar43619@wp.pl>
2025-10-22 09:06:20 +0200

    backend index controle implementations with control an its view

---
+++ backend/packages/ulp/core/src/View/FormFields/Extra/Fields/IndexControl.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ulp\Core\View\FormFields\Extra\Fields;
+
+class IndexControl extends \Ulp\Core\View\FormFields\Extra\BaseExtraField {  
+
+  protected array $labels;
+  protected array $data;
+  protected array $filterable;
+  protected string $destinations;
+
+  public function __construct(array $data) {
+    parent::__construct($data);
+    $this->labels = $data['labels'];
+    $this->data = $data['data'] 
+      instanceof \Illuminate\Database\Eloquent\Collection
+      ? $data['data']->toArray()
+      : $data['data'];
+    $this->filterable = $data['filterable'];
+    $this->destinations = $data['destinations'];
+  }
+
+  protected function resolveView(): string {
+    return 'core::components.form_fields.cms_extra.intex';
+  }
+}
