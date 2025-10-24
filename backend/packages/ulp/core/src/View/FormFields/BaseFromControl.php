3a794c547028335a7580f24633971f46866d51ec
OskarP <oskar43619@wp.pl>
2025-10-24 09:39:15 +0200

    backend packages core add base klass fore form elements and som dock for it

---
+++ backend/packages/ulp/core/src/View/FormFields/BaseFromControl.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ulp\Core\View\FormFields;
+
+/**
+ * Abstract class BaseFromControl
+ * The base class for pools used in the form and only by them should be inherited. 
+ * Fields such as those from the Extra or Putton access don't need this, so 
+ * they inherit directly from BaseControl because they don't need the following 
+ * attributes. The auxiliary class was created for cleaner, better architecture.
+ */
+abstract class BaseFromControl extends BaseControl {
+
+  protected string $name;
+  protected string $label;
+  protected string $tooltip; // To implemnt Iplement in blade 
+  protected bool $readonly;
+  protected bool $required;
+  protected array $validation; // To implemnt Iplement in blade 
+  protected string $wraper;
+
+  public function __construct(array $data) {
+    parent::__construct($data);
+    $this->name = $data['name'] ?? '';
+    $this->label = $data['label'] ?? '';
+    $this->tooltip = $data['tooltip'] ?? '';
+    $this->readonly = $data['readonly'] ?? false;
+    $this->required = $data['required'] ?? false;
+    $this->validation = $data['validation'] ?? []; 
+    $this->wraper = $data['wraper'] ?? 'flex flex-col w-full md:w-[32%]';
+  }
+
+}
