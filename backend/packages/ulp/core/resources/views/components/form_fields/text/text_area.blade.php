3e2a7bb53b89d9eb98cd052c2d2782d895c9ca47
OskarP <oskar43619@wp.pl>
2025-10-25 23:26:30 +0200

    backend core checkbox and text_area som fix

--- backend/packages/ulp/core/resources/views/components/form_fields/text/text_area.blade.php
+++ backend/packages/ulp/core/resources/views/components/form_fields/text/text_area.blade.php
@@ -1,14 +1,14 @@
 <div x-data="{ 
     label: {{ json_encode($label) }},
     name: {{ json_encode($name) }},
-    width: {{ json_encode($width) }},
+    wraper: {{ json_encode($wraper) }},
     value: {{ json_encode($value) }}, 
     readonly: {{ json_encode($readonly) }},
     tooltip: {{ json_encode($tooltip) }},
     validation: {{ json_encode($validation) }},
   }"
   class="mb-3 relative"
-  :class="width">
+  :class="wraper">
   <label :for="name" 
     class="flex text-sm font-medium text-gray-400 ml-2 mb-1 items-center gap-2">
     <span x-text="label"></span>
