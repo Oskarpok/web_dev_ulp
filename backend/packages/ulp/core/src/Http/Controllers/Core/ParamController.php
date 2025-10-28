4b8e2c3d1474845de54f3644578ea082c58c0f71
OskarP <oskar43619@wp.pl>
2025-10-28 23:41:34 +0100

    Update ParamController.php

--- backend/packages/ulp/core/src/Http/Controllers/Core/ParamController.php
+++ backend/packages/ulp/core/src/Http/Controllers/Core/ParamController.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Ulp\Core\Http\Controllers\Core;
 
-use Illuminate\Http\Request;
+use Ulp\Core\Enums\ParamsType;
 use Illuminate\Support\Facades\Route;
 use Ulp\Core\View\FormFields\Text\TextTypeController;
+use Ulp\Core\View\FormFields\Select\SelectTypeControl;
+use Ulp\Core\View\FormFields\DateTime\DateTimeTypeControl;
 
 class ParamController extends \Ulp\Core\Http\Controllers\BaseController {
 
@@ -22,7 +24,7 @@ class ParamController extends \Ulp\Core\Http\Controllers\BaseController {
     ];
   }
 
-  protected function indexPrepare(Request $request): array {
+  protected function indexPrepare(\Illuminate\Http\Request $request): array {
     return [
       'data' => self::MODEL_CLASS::filter($request, [
         'id', 'name', 'type', 'value', 'created_at', 'updated_at',
@@ -57,11 +59,93 @@ class ParamController extends \Ulp\Core\Http\Controllers\BaseController {
         'name' => 'name',
         'label' => 'Nazwa',
         'value' => $data?->name,
-        'required' => true,
-        'validation' => $validationRules['name'],
+        'required' => in_array('required', $validationRules['name']),
+        'readonly' => $currentRoute !== self::ROUTE_NAME . 'show' 
+          ? false : true,
       ]),
-
-
+      new SelectTypeControl([
+        'type' => 'select',
+        'name' => 'type',
+        'label' => 'Type',
+        'options' => ParamsType::toArray() ?? [],
+        'required' => in_array('required', $validationRules['type']),
+        'value' => $data?->type,
+        'readonly' => $currentRoute !== self::ROUTE_NAME . 'show' 
+          ? false : true,
+      ]),
+      (function($data, $currentRoute) {
+        return match ($data?->type) {
+          1 => new TextTypeController([
+            'type' => 'number',
+            'name' => 'val_int',
+            'label' => 'Value',
+            'value' => $data?->val_int,
+            'required' => true,
+            'readonly' => $currentRoute !== self::ROUTE_NAME . 'show' 
+              ? false : true,
+          ]),
+          2 => new TextTypeController([
+            'type' => 'number',
+            'name' => 'val_float',
+            'label' => 'Value',
+            'value' => $data?->val_float,
+            'readonly' => true,
+            'allow_float' => true,
+            'required' => true,
+            'readonly' => $currentRoute !== self::ROUTE_NAME . 'show' 
+              ? false : true,
+          ]),
+          4 => new SelectTypeControl([
+            'type' => 'checkbox',
+            'name' => 'val_bool',
+            'label' => 'Value',
+            'required' => true,
+            'value' => $data?->val_bool,
+            'readonly' => $currentRoute !== self::ROUTE_NAME . 'show' 
+              ? false : true,
+          ]),
+          5 => new TextTypeController([
+            'type' => 'text_area',
+            'name' => 'val_json',
+            'label' => 'Value',
+            'required' => true,
+            'value' => $data?->val_json,
+            'readonly' => $currentRoute !== self::ROUTE_NAME . 'show' 
+              ? false : true,
+          ]),
+          default => new TextTypeController([
+            'type' => 'text',
+            'name' => 'val_string',
+            'label' => 'Value',
+            'value' => $data?->val_string,
+            'required' => true,
+            'readonly' => $currentRoute !== self::ROUTE_NAME . 'show' 
+              ? false : true,
+          ]),
+        };
+      })($data, $currentRoute),
+      (function($currentRoute, $created_at) {
+        if($currentRoute !== self::ROUTE_NAME . 'create') {
+          return new DateTimeTypeControl([
+            'type' => 'date_time',
+            'name' => 'created_at',
+            'label' => 'Utworzony',
+            'readonly' => true,
+            'value' => $created_at,
+          ]);
+        }
+      })($currentRoute, $data?->created_at),
+      (function($currentRoute, $updated_at) {
+        if($currentRoute !== self::ROUTE_NAME . 'create') {
+          return new DateTimeTypeControl([
+            'type' => 'date_time',
+            'name' => 'updated_at',
+            'label' => 'Zaktualizowany',
+            'readonly' => true,
+            'value' => $updated_at,
+          ]);
+        }
+      })($currentRoute, $data?->updated_at),
     ];
   }
   
