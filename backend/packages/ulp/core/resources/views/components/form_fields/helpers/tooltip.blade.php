ea8a39c4374864f24293c6470e2e1d4d1ad8231b
OskarP <oskar43619@wp.pl>
2025-11-01 20:35:13 +0100

    Update tooltip.blade.php

--- backend/packages/ulp/core/resources/views/components/form_fields/helpers/tooltip.blade.php
+++ backend/packages/ulp/core/resources/views/components/form_fields/helpers/tooltip.blade.php
@@ -1,1 +1,16 @@
-tooltip
+@props([
+  'tooltip' => ''
+])
+
+@if(!empty($tooltip))
+<div class="relative group">
+  <i class="fa-solid fa-circle-question text-gray-400 cursor-pointer text-xs"></i>
+  <div class="absolute top-1/2 left-full ml-2 -translate-y-1/2 z-10 w-max 
+    px-2 py-1 bg-gray-800 text-gray-200 text-xs rounded shadow-lg 
+    opacity-0 group-hover:opacity-100 transition-opacity text-left">
+    <template x-for="line in '{{ $tooltip }}'.split('|')" :key="line">
+      <div x-text="line.trim()"></div>
+    </template>
+  </div>
+</div>
+@endif
