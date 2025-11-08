7dc0b7bf75cc9fc02e57c252f0cbce6a0a7a59cf
OskarP <oskar43619@wp.pl>
2025-11-08 21:56:38 +0100

    backend core orders init controler and model

--- backend/packages/ulp/core/src/Http/Controllers/Core/OrderController.php
+++ backend/packages/ulp/core/src/Http/Controllers/Core/OrderController.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 namespace Ulp\Core\Http\Controllers\Core;
+use Ulp\Core\View\FormFields\Text\TextTypeController;
+use Ulp\Core\View\FormFields\DateTime\DateTimeTypeControl;
 
 class OrderController extends \Ulp\Core\Http\Controllers\BaseController {
 
@@ -11,13 +13,27 @@ class OrderController extends \Ulp\Core\Http\Controllers\BaseController {
 
   protected function titles(): array {
     return [
-      //
+      'index' => 'Orders Panel',
+      'create' => 'Orders Create Panel',
+      'edit' => 'Orders Edit Panel',
+      'show' => 'Orders Show Panel',
     ];
   }
 
   protected function indexPrepare(\Illuminate\Http\Request $request): array {
     return [
-      //
+      'data' => self::MODEL_CLASS::filter($request, [
+        'id', 'user_id', 'price', 'status', 'parcel_number',  
+        'created_at', 'updated_at',
+      ])->get(), 
+      'labels' => [
+        'Id', 'user_id', 'price', 'status', 'parcel_number', 
+        'Created at', 'Updated at',
+      ],
+      'filterable' => [
+        'id' => true, 'user_id' => true, 'price' => false, 'status' => false,
+        'parcel_number' => true, 'created_at' => true, 'updated_at' => true,
+      ],
     ];
   }
 
@@ -25,7 +41,44 @@ class OrderController extends \Ulp\Core\Http\Controllers\BaseController {
     $currentRoute = \Illuminate\Support\Facades\Route::currentRouteName();
     $validationRules = self::MODEL_CLASS::validationRules();
     return [
-      //
+      (function($currentRoute, $id) {
+        if($currentRoute !== self::ROUTE_NAME . 'create') {
+          return TextTypeController::make([
+            'type' => 'number',
+            'name' => 'id',
+            'label' => 'ID',
+            'value' => $id,
+            'readonly' => true,
+          ]);
+        }
+      })($currentRoute, $data?->id),
+      
+
+
+
+      (function($currentRoute, $created_at) {
+        if($currentRoute !== self::ROUTE_NAME . 'create') {
+          return DateTimeTypeControl::make([
+            'type' => 'date_time',
+            'name' => 'created_at',
+            'label' => 'Utworzony',
+            'readonly' => true,
+            'value' => $created_at,
+          ]);
+        }
+      })($currentRoute, $data?->created_at),
+      (function($currentRoute, $updated_at) {
+        if($currentRoute !== self::ROUTE_NAME . 'create') {
+          return DateTimeTypeControl::make([
+            'type' => 'date_time',
+            'name' => 'updated_at',
+            'label' => 'Zaktualizowany',
+            'readonly' => true,
+            'value' => $updated_at,
+          ]);
+        }
+      })($currentRoute, $data?->updated_at),
+
     ];
   }
 
--- backend/packages/ulp/core/src/Models/Core/Order.php
+++ backend/packages/ulp/core/src/Models/Core/Order.php
@@ -7,18 +7,31 @@ namespace Ulp\Core\Models\Core;
 class Order extends \Ulp\Core\Models\Base {
 
   protected $fillable = [
-    'price', 'products', 'parcel_number', 'status', 'notes',
+    'user_id', 'price', 'status', 'parcel_number', 'notes',
   ];
 
   public function products() {
     return $this->belongsToMany(Product::class)
-      ->withPivot(['name', 'unit_price',])
-      ->withTimestamps();
+      ->withPivot([
+        'name', 'unit_price', 'is_active', 'specification', 'description',
+      ])->withTimestamps();
   }
+
+  // protected static function booted() {
+  //   static::updating(function ($order) {
+  //     if ($order->isDirty('user_id')) {
+  //       throw new \Exception('Pole user_id nie może być modyfikowane po utworzeniu zamówienia.');
+  //     }
+  //   });
+  // }
 
   public static function validationRules($id = null): array {
     return [
-      //
+      'user_id' => ['',],
+      'price' => ['',],
+      'parcel_number' => ['',],
+      'status' => ['',],
+      'notes' => ['',],
     ];
   }
 
