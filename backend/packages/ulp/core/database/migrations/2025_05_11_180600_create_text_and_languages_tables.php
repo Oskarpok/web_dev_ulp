344eb7d0417c639f4b9a5b7f61ec6e2eaa7fb8a4
OskarP <oskar43619@wp.pl>
2025-11-09 10:22:53 +0100

    Create 2025_05_11_180600_create_text_and_languages_tables.php

---
+++ backend/packages/ulp/core/database/migrations/2025_05_11_180600_create_text_and_languages_tables.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends \Illuminate\Database\Migrations\Migration {
+
+  /**
+   * Run the migrations.
+   */
+  public function up(): void {
+    Schema::create('texts', function (Blueprint $table) {
+      $table->id();
+      $table->string('name')->unique();
+      $table->timestamps();
+    });
+
+    Schema::create('languages', function (Blueprint $table) {
+      $table->id();
+      $table->string('name')->unique();
+      $table->string('shortcut')->unique();
+      $table->boolean('is_active')->default(false);
+      $table->timestamps();
+    });
+
+    Schema::create('text_translations', function (Blueprint $table) {
+      $table->id();
+      $table->foreignId('text_id')->constrained()->cascadeOnDelete();
+      $table->foreignId('language_id')->constrained()->cascadeOnDelete();
+      $table->text('translation');
+      $table->timestamps();
+      $table->unique(['text_id', 'language_id']);
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void {
+    Schema::dropIfExists('texts');
+    Schema::dropIfExists('languages');
+    Schema::dropIfExists('text_translations');
+  }
+
+};
