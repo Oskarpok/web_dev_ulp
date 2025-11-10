6d27537b005fce78d1bfa35e22bd5bc8e8afc61d
OskarP <oskar43619@wp.pl>
2025-11-11 00:07:56 +0100

    Update Text.php

--- backend/packages/ulp/core/src/Models/Api/Text.php
+++ backend/packages/ulp/core/src/Models/Api/Text.php
@@ -6,14 +6,21 @@ namespace Ulp\Core\Models\Api;
 
 class Text extends \Ulp\Core\Models\Base {
 
-  protected $fillable = [
-    //
-  ];
+  protected $fillable = ['name',];
 
   public static function validationRules($id = null): array {
     return [
-      //
+      'name' => ['required', 'string', 'max:255',
+        \Illuminate\Validation\Rule::unique('name')->ignore($id)],
+      'translations.*' => ['nullable', 'string'],
     ];
+  }
+
+  public function languages() {
+    return $this->belongsToMany(Language::class, 
+      'text_translations', 'text_id', 'language_id')
+      ->withPivot(['translation'])
+      ->withTimestamps();
   }
 
 }
