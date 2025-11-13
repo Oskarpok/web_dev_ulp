1670984bec2b29a05cd6702501e092c0f3d6dd0e
OskarP <oskar43619@wp.pl>
2025-11-13 16:10:00 +0100

    Update web.php

--- backend/packages/ulp/core/routes/web.php
+++ backend/packages/ulp/core/routes/web.php
@@ -3,17 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->name('core.')
-  ->namespace('\Ulp\Core\Http\Controllers\Core')
-  ->group(function () {
+  ->namespace('\Ulp\Core\Http\Controllers\Core')->group(function () {
     Route::resource('users', 'Users\UserController');
     Route::resource('params', 'System\ParamController');
     Route::resource('orders', 'Production\OrderController');
     Route::resource('products', 'Production\ProductController');
-});
-
-Route::prefix('admin')->name('core.')
-  ->namespace('\Ulp\Core\Http\Controllers\Core\Front')
-  ->group(function () {
-    Route::resource('languages', 'LanguagesController');
-    Route::resource('texts', 'TextsController');
+    Route::resource('languages', 'Front\LanguagesController');
+    Route::resource('texts', 'Front\TextsController');
 });
