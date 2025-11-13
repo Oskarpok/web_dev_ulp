8f9c6b91f8d650bd607d9a71100eb39d6287e47d
OskarP <oskar43619@wp.pl>
2025-11-13 21:04:52 +0100

    asdasd

--- backend/packages/ulp/core/resources/views/layouts/app.blade.php
+++ backend/packages/ulp/core/resources/views/layouts/app.blade.php
@@ -20,23 +20,20 @@
         <div class="flex flex-col items-center py-4 border-b border-gray-700">
           <div class="flex items-center justify-center text-center">
             <span class="font-semibold break-words leading-tight">
-              user first_name user sur_name
+              {{ auth()->user()->first_name }} {{ auth()->user()->sur_name }}
             </span>
           </div>
           <span class="text-sm text-gray-400">
-            usertype
+             {{ auth()->user()->type_label }}
           </span>
         </div>
-
+        
         <nav class="flex-1 overflow-y-auto px-4 py-4 space-y-6 text-sm">
-          <div>
-            <h3 class="text-gray-500 uppercase tracking-wide mb-2">Dashboard</h3>
-            <a href="#" class="flex items-center px-2 py-2 bg-gray-800 rounded 
-              core-secondary-color">
-              <span>Dashboard</span>
-            </a>
-          </div>
-            naw elents
+
+            
+
+
+
         </nav>
       </aside>
 			<div class="flex flex-col flex-1">
