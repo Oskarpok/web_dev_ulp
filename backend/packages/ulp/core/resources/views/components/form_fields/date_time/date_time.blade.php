7251cd4b939fd1041d022fe41f566b14c65979ae
OskarP <oskar43619@wp.pl>
2025-12-22 21:23:45 +0100

    Update date_time.blade.php

--- backend/packages/ulp/core/resources/views/components/form_fields/date_time/date_time.blade.php
+++ backend/packages/ulp/core/resources/views/components/form_fields/date_time/date_time.blade.php
@@ -1,14 +1,5 @@
-<div 
-  x-data="{ 
-    name: {{ json_encode($name) }},
-    value: {{ json_encode($value) }}, 
-    readonly: {{ json_encode($readonly) }},
-    tooltip: {{ json_encode($tooltip) }},
-    validation: {{ json_encode($validation) }},
-  }"
-  class="mb-3 relative {{ $wraper }}">
-  <label :for="name"  
-    class="block text-sm font-medium text-gray-400 mb-1">
+<div class="mb-3 relative {{ $wraper }}">
+  <label class="block text-sm font-medium text-gray-400 mb-1">
     <span>{{ $label }}</span>
     @component('core::components.form_fields.helpers.tooltip', [
       'tooltip' => $tooltip,
@@ -16,12 +7,11 @@
     @endcomponent
   </label>
   <input type="datetime-local" 
-    :id="name"
-    :name="name"
-    :readonly="readonly"
-    :value="value" 
+    name="{{ $name }}"
+    @readonly($readonly)
     @required($required)
+    value="{{ $value }}"
     class="mt-1 w-full border border-gray-600 rounded-xl px-3 py-2 
-    text-gray-300 shadow-inner cursor-default focus:outline-none"
-    :class="readonly ? 'bg-[#1e293b]' : '' ">
+    text-gray-300 shadow-inner cursor-default focus:outline-none
+      {{ $readonly ? 'bg-[#1e293b]' : '' }}">
 </div>
