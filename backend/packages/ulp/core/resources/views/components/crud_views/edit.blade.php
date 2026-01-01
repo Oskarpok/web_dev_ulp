5a772606c3f05ea1afc1bd96655316ca3e311e30
OskarP <oskar43619@wp.pl>
2026-01-01 09:45:16 +0100

    backend core creat and edit prep for form luvuaire components

--- backend/packages/ulp/core/resources/views/components/crud_views/edit.blade.php
+++ backend/packages/ulp/core/resources/views/components/crud_views/edit.blade.php
@@ -1,21 +1,21 @@
 @extends('core::layouts.app')
 @section('content')
 <div class="bg-gray-950 p-6 text-gray-200 rounded-2xl">
-  <form method="POST" action="{{ $route }}">
+  <div class="flex items-center justify-between mb-6 p-4 
+    bg-[core-background-color] rounded-lg shadow">
+    <h1 class="text-xl font-semibold py-2">
+      {!! $title !!}
+    </h1>
+    <div class="flex gap-x-2">
+      @foreach ($controls['buttons'] as $button)
+        @continue(is_null($button))
+        {!! $button->render() !!}
+      @endforeach
+    </div>
+  </div>
+  <form id="form" method="POST" action="{{ $route }}">
     @csrf
     @method('PUT')
-    <div class="flex items-center justify-between mb-6 p-4 
-      bg-[core-background-color] rounded-lg shadow">
-      <h1 class="text-xl font-semibold py-2">
-        {!! $title !!}
-      </h1>
-      <div class="flex gap-x-2">
-        @foreach ($controls['buttons'] as $button)
-          @continue(is_null($button))
-          {!! $button->render() !!}
-        @endforeach
-      </div>
-    </div>
     <div class="w-full mx-auto p-6 shadow rounded-2xl space-y-6 text-gray-200 
       border border-gray-600 flex flex-wrap gap-5 place-items-center">
       @foreach ($controls['fields'] as $field)
