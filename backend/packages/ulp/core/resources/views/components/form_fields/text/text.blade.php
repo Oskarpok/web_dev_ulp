a39698a2b85cae3e1a0d287d09f3a804db003387
OskarP <oskar43619@wp.pl>
2026-01-03 13:35:40 +0100

    backend core emial  text and date time add validation compoenents elenets to its vue

--- backend/packages/ulp/core/resources/views/components/form_fields/text/text.blade.php
+++ backend/packages/ulp/core/resources/views/components/form_fields/text/text.blade.php
@@ -7,13 +7,21 @@
     ])
     @endcomponent
   </label>
+  @error('state.' . $name)
+    <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
+  @enderror
   <input type="text" 
     value="{{ $value }}"
     name="{{ $name }}"
     @readonly($readonly)
     @required($required)
     @disabled($disabled)
-    class="mt-1 w-full border border-gray-600 rounded-xl px-3 py-2 
-      text-gray-300 shadow-inner focus:outline-none
-      {{ $readonly ? 'bg-[#1e293b] cursor-default' : '' }}">
+    wire:model.blur="state.{{ $name }}"
+    @class([
+      'mt-1 w-full border rounded-xl px-3 py-2 text-gray-300 shadow-inner focus:outline-none',
+      'bg-[#1e293b] cursor-default' => $readonly,
+      'border-red-500' => $errors->has('state.' . $name),
+      'border-gray-600' => ! $errors->has('state.' . $name),
+    ])
+  >
 </div>
