7d2156967e771827c46bee55f45b07b9e3bc3b82
OskarP <oskar43619@wp.pl>
2026-01-06 16:36:55 +0100

    Update number.blade.php

--- backend/packages/ulp/core/resources/views/components/form_fields/text/number.blade.php
+++ backend/packages/ulp/core/resources/views/components/form_fields/text/number.blade.php
@@ -12,18 +12,12 @@
 </style>
 <div 
   x-data="{ 
-    name: {{ json_encode($name) }},
     value: {{ json_encode($value) }},
     step: {{ json_encode($step) }}, 
-    readonly: {{ json_encode($readonly) }},
-    max: {{ json_encode($max) }},
-    min: {{ json_encode($min) }},
     allowFloat: {{ json_encode($allow_float) }},
-    validation: {{ json_encode($validation) }},
   }"
   class="mb-3 relative {{ $wraper }}">
-  <label :for="name" 
-    class="flex text-sm font-medium text-gray-400 mb-1 items-center gap-2">
+  <label class="flex text-sm font-medium text-gray-400 mb-1 items-center gap-2">
     <span>{{ $label }}</span>
     @component('core::components.form_fields.helpers.tooltip', [
       'tooltip' => $tooltip,
@@ -31,22 +25,17 @@
     @endcomponent
   </label>
   <div class="flex items-center border border-gray-600 rounded-xl 
-    shadow-inner mt-1"
-    :class="[
-      readonly ? 'bg-[#1e293b]' : '',
-      false ? 'border-red-500' : 'border-gray-600',
-    ]">
-    <input type="text"
+    shadow-inner mt-1 {{ $readonly ? 'bg-[#1e293b]' : '' }}">
+    <input type="number"
       x-model="value"
       class="w-full bg-transparent text-gray-300 px-3 py-2 rounded-l-xl 
-        focus:outline-none"
-      :class="readonly ? 'cursor-default' : ''"
-      :id="name"
-      :name="name"
-      :readonly="readonly"
+        focus:outline-none {{ $readonly ? 'cursor-default' : '' }}"
+      name="{{ $name }}"
+      @readonly($readonly)
       @required($required)
-      :max="max"
-      :min="min"
+      @disabled($disabled)
+      max="{{ $max }}"
+      min="{{ $min }}"
       inputmode="decimal"
       @keydown="
         const key = $event.key;
@@ -75,18 +64,18 @@
     <div class="flex flex-col">
       <button type="button"
         @click="value += step"
-        class="text-[#898d95] px-1 py-0.5 text-xs rounded-tr-xl"
-        :class="readonly ? 'cursor-default' : 'hover:text-[#d1d5dc]' "
+        class="text-[#898d95] px-1 py-0.5 text-xs rounded-tr-xl
+          {{ $readonly ? 'cursor-default' : 'hover:text-[#d1d5dc]' }}"
         aria-label="Increment"
-        :disabled="readonly">
+        @disabled($readonly)>
         ▲
       </button>
       <button type="button"
         @click="value -= step"
-        class="text-[#898d95] px-1 py-0.4 text-xs rounded-br-xl"
-        :class="readonly ? 'cursor-default' : 'hover:text-[#d1d5dc]' "
+        class="text-[#898d95] px-1 py-0.4 text-xs rounded-br-xl
+          {{ $readonly ? 'cursor-default' : 'hover:text-[#d1d5dc]' }}"
         aria-label="Decrement"
-        :disabled="readonly">
+        @disabled($readonly)>
         ▼
       </button>
     </div>
