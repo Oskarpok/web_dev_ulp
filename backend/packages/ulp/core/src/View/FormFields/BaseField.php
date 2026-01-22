98919bf06f3ee8e81dd56632edd48dfde496b791
OskarP <oskar43619@wp.pl>
2026-01-22 19:25:58 +0100

    Update BaseField.php

--- backend/packages/ulp/core/src/View/FormFields/BaseField.php
+++ backend/packages/ulp/core/src/View/FormFields/BaseField.php
@@ -48,8 +48,8 @@ abstract class BaseField {
    * 
    * @return string HTML of the field
    */
-  public function render(array $Data = []): string {
-    return view($this->resolveView(), get_object_vars($this) + $Data)->render();
+  public function render(array $data = []): string {
+    return view($this->resolveView(), get_object_vars($this) + $data)->render();
   }
 
 }
