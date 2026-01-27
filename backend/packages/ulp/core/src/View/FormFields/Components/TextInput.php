6eb012fea304e523167d9284e16409bf1a82c206
OskarP <oskar43619@wp.pl>
2026-01-27 23:24:22 +0100

    Update TextInput.php

--- backend/packages/ulp/core/src/View/FormFields/Components/TextInput.php
+++ backend/packages/ulp/core/src/View/FormFields/Components/TextInput.php
@@ -7,20 +7,46 @@ namespace Ulp\Core\View\FormFields\Components;
 use Ulp\Core\View\FormFields\Components\Traits\BeDisabled;
 use Ulp\Core\View\FormFields\Components\Traits\BeReadonly;
 use Ulp\Core\View\FormFields\Components\Traits\BeRequired;
+use Ulp\Core\View\FormFields\Components\Traits\HasAutocomplete;
 
 class TextInput extends Input {
 
-  use BeDisabled, BeReadonly, BeRequired;
+  use BeDisabled, BeReadonly, BeRequired, HasAutocomplete;
 
-  protected string $value;
-
-  public function value(string $value): static {
-    $this->value = $value;
-    return $this;
-  }
+  protected string $type = 'text';
 
   protected function resolveView(): string {
     return 'core::components.form_fields.text.text';
   }
-  
+
+  public function email() {
+    $this->type = 'email';  
+    return $this;
+  }
+
+  public function numeric() {
+    $this->type = 'numeric';  
+    return $this;
+  }
+
+  public function integer() {
+    $this->type = 'integer';  
+    return $this;
+  }
+
+  public function password() {
+    $this->type = 'password';  
+    return $this;
+  }
+
+  public function tel() {
+    $this->type = 'tel';  
+    return $this;
+  }
+
+  public function url() {
+    $this->type = 'url';  
+    return $this;
+  }
+
 }
