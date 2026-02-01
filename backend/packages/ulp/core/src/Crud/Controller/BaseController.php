9237aae6fd0ef3d2ca39bab3fc8ad0919e88c4f1
OskarP <oskar43619@wp.pl>
2026-02-01 13:10:49 +0100

    Update BaseController.php

--- backend/packages/ulp/core/src/Crud/Controller/BaseController.php
+++ backend/packages/ulp/core/src/Crud/Controller/BaseController.php
@@ -4,14 +4,68 @@ declare(strict_types=1);
 
 namespace Ulp\Core\Crud\Controller;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Ulp\Core\Crud\Concerns\CrudeMethods;
 use Ulp\Core\Crud\Concerns\BaseCrudeFields;
+use Ulp\Core\View\FormFields\Buttons\ButtonsTypeController;
 
 
 abstract class BaseController extends \Illuminate\Routing\Controller {
 
   use CrudeMethods, BaseCrudeFields;
+
+  /**
+   * Return an array of vie index requaier data.
+   *
+   * @return array List of fields elements for the given controller.
+   */
+  abstract protected function indexTable(Request $request): array;
+
+  //
+  public function heckRowButtonsAcces($destination): array {
+    return [
+      'show' => Route::has($destination . 'show'),
+      'edit' => Route::has($destination . 'edit'),
+      'destroy' => Route::has($destination . 'destroy'),
+    ];
+  }
+
+    // Prepare buttobs for index vievs
+  protected function prepareIndexButtons(): array {
+    return Route::has(static::ROUTE_NAME . 'create') ? [
+      ButtonsTypeController::make([
+        'type' => 'anchore',
+        'routeName' => static::ROUTE_NAME . 'create',
+        'label' => $this->titles()['recordAddButton'] ?? 'Add',
+        'icone' => 'fa-solid fa-plus',
+      ]),
+    ] : [];
+  }
+
+  /**
+   * Prepares the data for the index view by calling the indexTable()
+   * method with the current request, and then returns the corresponding
+   * Blade view with the prepared data.
+   *
+   * @param \Illuminate\Http\Request $request  The incoming HTTP request.
+   * @return \Illuminate\View\View  The rendered index view.
+   */
+  public function index(Request $request): \Illuminate\View\View {
+    $data = $this->indexTable($request);
+    return view(self::CRUD_VIEWS . 'index', [
+      'title' => $this->titles()['index'] ?? '',
+      'buttons' => $this->prepareIndexButtons(),
+      'table' => new \Ulp\Core\View\FormFields\Extra\Fields\IndexControl([
+        'type' => 'intex',
+        'labels' => $data['labels'],
+        'filterable' => $data['filterable'],
+        'data' => $data['data'],
+        'destinations' => static::ROUTE_NAME,
+        'resolveButtons' => $this->heckRowButtonsAcces(static::ROUTE_NAME),
+      ]),
+    ]);
+  }
 
   /**
    * Prepares widowed crud form elements based on their specific fields, 
@@ -30,6 +84,28 @@ abstract class BaseController extends \Illuminate\Routing\Controller {
         ...$this->formFieldsButtons(Route::currentRouteName()),
       ],
     ];
+  }
+
+  // Preper submit button for crud operations
+  protected function formFieldsButtons($currentRoute) {
+    $buttons = [];
+
+    if ($currentRoute !== static::ROUTE_NAME . 'show') {
+      $buttons[] = ButtonsTypeController::make([
+        'type' => 'submit',
+        'label' => 'Save',
+        'icone' => 'fa-solid fa-file',
+      ]);
+    }
+
+    $buttons[] = ButtonsTypeController::make([
+      'type' => 'anchore',
+      'routeName' => static::ROUTE_NAME . 'index',
+      'label' => 'Return',
+      'icone' => 'fa-solid fa-arrow-left',
+    ]);
+
+    return $buttons;
   }
 
 }
