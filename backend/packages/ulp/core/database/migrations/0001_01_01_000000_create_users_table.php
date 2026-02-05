eec708f158501ed28a39d4931c498e6aaa9578d4
OskarP <oskar43619@wp.pl>
2026-02-05 20:27:27 +0100

    Update 0001_01_01_000000_create_users_table.php

--- backend/packages/ulp/core/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/packages/ulp/core/database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,16 +11,34 @@ return new class extends \Illuminate\Database\Migrations\Migration {
 	public function up(): void {
 		Schema::create('users', function (Blueprint $table) {
 			$table->id();
-			$table->string('first_name');
-			$table->string('sur_name');
 			$table->string('phone')->unique();
 			$table->string('email')->unique();
 			$table->string('password');
 			$table->integer('type')->default(0);
 			$table->boolean('is_active')->default(false);
 			$table->timestamp('email_verified_at')->nullable();
+			$table->string('street')->nullable();
+			$table->string('city')->nullable();
+			$table->string('postcode', 6)->nullable();
 			$table->rememberToken();
 			$table->timestamps();
+		});
+
+		Schema::create('person_details', function (Blueprint $table) {
+			$table->id();
+			$table->foreignId('user_id')->constrained()->onDelete('cascade');
+			$table->string('first_name');
+			$table->string('sur_name');
+			$table->string('pesel', 11)->nullable()->unique();
+		});
+
+		Schema::create('company_details', function (Blueprint $table) {
+			$table->id();
+			$table->foreignId('user_id')->constrained()->onDelete('cascade');
+			$table->string('company_name');
+			$table->string('nip', 10);
+			$table->string('regon', 14)->nullable();
+			$table->string('krs', 10)->nullable();
 		});
 
 		Schema::create('password_reset_tokens', function (Blueprint $table) {
