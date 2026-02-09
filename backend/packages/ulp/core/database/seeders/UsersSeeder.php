0d726c0447728e79c343fadd15c15b7b46072441
OskarP <oskar43619@wp.pl>
2026-02-09 22:05:27 +0100

    Update UsersSeeder.php

--- backend/packages/ulp/core/database/seeders/UsersSeeder.php
+++ backend/packages/ulp/core/database/seeders/UsersSeeder.php
@@ -11,23 +11,6 @@ class UsersSeeder extends \Illuminate\Database\Seeder {
    * Seed the application's database for users table.
    */
   public function run(): void {
-    User::create([
-      'first_name' => 'admin',
-      'sur_name' => 'admin_admin',
-      'phone' => '48234567891',
-      'type' => UsersType::Administrator->value,
-      'email' => 'admin@example.com',
-      'password' => Hash::make('admin!1234'),
-      'is_active' => true,
-    ]);
-    User::create([
-      'first_name' => 'user',
-      'sur_name' => 'user_user',
-      'phone' => '48678912345',
-      'type' => UsersType::User->value,
-      'email' => 'user@example.com',
-      'password' => Hash::make('user!1234'),
-      'is_active' => true,
-    ]);
+
   }
 }
