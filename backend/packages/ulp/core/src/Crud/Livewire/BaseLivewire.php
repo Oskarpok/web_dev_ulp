6a65295dcb4adf23d7152c60596db4c712545124
OskarP <oskar43619@wp.pl>
2026-02-12 17:09:19 +0100

    Create BaseLivewire.php

---
+++ backend/packages/ulp/core/src/Crud/Livewire/BaseLivewire.php
@@ -0,0 +1,19 @@
+<?php
+ 
+namespace Ulp\Core\Crud\Livewire;
+ 
+/**
+ * Universal Livewire form component
+ *
+ * - Handles dynamic form fields render
+ * - Manages form state
+ * - Provides validation
+ * - Supports HTTP methods (GET, POST, PUT, PATCH)
+ */
+class BaseLivewire extends \Livewire\Component {
+  
+  use \Livewire\WithFileUploads;
+
+ 
+
+}
