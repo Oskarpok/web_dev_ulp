b3b713c270ed242aa410e70080b1e8dd8873adbe
OskarP <oskar43619@wp.pl>
2026-02-15 12:47:19 +0100

    backend core users resorces for form fields asnd som fix

---
+++ backend/packages/ulp/core/src/Livewire/Resources/Core/Users/UserResources.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ulp\Core\Livewire\Resources\Core\Users;
+
+use Ulp\Core\View\FormFields\Components\Checkbox;
+use Ulp\Core\View\FormFields\Components\TextInput;
+use Ulp\Core\View\FormFields\Components\DateTimePicker;
+use Ulp\Core\View\FormFields\Buttons\ButtonsTypeController;
+
+class UserResources extends \Ulp\Core\Crud\Livewire\BaseLivewire {
+
+  protected const ROUTE_NAME = 'core.users.';
+
+  public static function createButtons(): array {
+    return [
+      ButtonsTypeController::make([
+        'type' => 'submit',
+        'label' => 'Save',
+        'icone' => 'fa-solid fa-file',
+      ]),
+      ButtonsTypeController::make([
+        'type' => 'anchore',
+        'routeName' => static::ROUTE_NAME . 'index',
+        'label' => 'Return',
+        'icone' => 'fa-solid fa-arrow-left',
+      ]),
+    ];
+  }
+
+  public static function createFields(): array {
+    return [
+      TextInput::make('phone')->tel()->label('Phone')->required(),
+      TextInput::make('email')->email()->label('Email')->required(),
+      TextInput::make('password')->password()->label('Password')->required(),
+      Checkbox::make('is_active ')->label('Is Active'),
+    ];
+  }
+
+  public static function showButtons(): array {
+    return [
+      ButtonsTypeController::make([
+        'type' => 'anchore',
+        'routeName' => static::ROUTE_NAME . 'index',
+        'label' => 'Return',
+        'icone' => 'fa-solid fa-arrow-left',
+      ]),
+    ];
+  }
+
+  public static function showFields(): array {
+    return [
+      TextInput::make('id')->label('Id')->numeric()->readonly(),
+      TextInput::make('phone')->tel()->label('Phone')->required()->readonly(),
+      TextInput::make('email')->email()->label('Email')->required()->readonly(),
+      TextInput::make('password')->password()->label('Password')->required()->readonly(),
+      Checkbox::make('is_active ')->label('Is Active')->disabled(),
+      DateTimePicker::make('created_at')->label('Created At')->readonly(),
+      DateTimePicker::make('updated_at')->label('Updated At')->readonly(),
+    ];
+  }
+
+  public static function editButtons(): array {
+    return [
+      ButtonsTypeController::make([
+        'type' => 'submit',
+        'label' => 'Save',
+        'icone' => 'fa-solid fa-file',
+      ]),
+      ButtonsTypeController::make([
+        'type' => 'anchore',
+        'routeName' => static::ROUTE_NAME . 'index',
+        'label' => 'Return',
+        'icone' => 'fa-solid fa-arrow-left',
+      ]),
+    ];
+  }
+
+  public static function editFields(): array {
+    return [
+      TextInput::make('id')->label('Id')->numeric()->readonly(),
+      TextInput::make('phone')->tel()->label('Phone')->required(),
+      TextInput::make('email')->email()->label('Email')->required(),
+      TextInput::make('password')->password()->label('Password')->required(),
+      Checkbox::make('is_active ')->label('Is Active'),
+      DateTimePicker::make('created_at')->label('Created At')->readonly(),
+      DateTimePicker::make('updated_at')->label('Updated At')->readonly(),
+    ];
+  }
+
+}
