12e88abe09549b27ddc8c23a3bf6597f641bee04
OskarP <oskar43619@wp.pl>
2026-02-16 11:12:00 +0100

    Update UserResources.php

--- backend/packages/ulp/core/src/Http/Resources/Core/Users/UserResources.php
+++ backend/packages/ulp/core/src/Http/Resources/Core/Users/UserResources.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Ulp\Core\Http\Resources\Core\Users;
 
+use Ulp\Core\Models\Core\Users\Role;
+use Ulp\Core\View\FormFields\Components\Select;
 use Ulp\Core\View\FormFields\Components\Checkbox;
 use Ulp\Core\View\FormFields\Components\TextInput;
 use Ulp\Core\View\FormFields\Components\DateTimePicker;
@@ -38,7 +40,7 @@ class UserResources extends \Ulp\Core\Crud\Resources\BaseResource {
     ];
   }
 
-  public static function createFields(): array {
+  public static function createFields($data = null): array {
     return [
       TextInput::make('phone')->tel()->label('Phone')->required(),
       TextInput::make('email')->email()->label('Email')->required(),
@@ -58,13 +60,23 @@ class UserResources extends \Ulp\Core\Crud\Resources\BaseResource {
     ];
   }
 
-  public static function showFields(): array {
+  public static function showFields($data = null): array {
     return [
-      TextInput::make('id')->label('Id')->numeric()->readonly(),
+      TextInput::make('id')->label('Id')->numeric()->readonly()->readonly(),
       TextInput::make('phone')->tel()->label('Phone')->required()->readonly(),
       TextInput::make('email')->email()->label('Email')->required()->readonly(),
       TextInput::make('password')->password()->label('Password')->required()->readonly(),
       Checkbox::make('is_active ')->label('Is Active')->disabled(),
+      Select::make('role')->label('Role')->searchable()->multiple()->required()
+        ->options(Role::pluck('name', 'id')->toArray())->disabled(),
+      ...array_map(fn($field) => $field->readonly(), 
+        match($data?->roles->first()?->details_table) {
+          'system_user_details' => self::systemUserFields(),
+          'company_details' => self::companyFields(),
+          'user_details' => self::regularUserFields(),
+          default => [],
+        }
+      ),
       DateTimePicker::make('created_at')->label('Created At')->readonly(),
       DateTimePicker::make('updated_at')->label('Updated At')->readonly(),
     ];
@@ -79,22 +91,61 @@ class UserResources extends \Ulp\Core\Crud\Resources\BaseResource {
       ]),
       ButtonsTypeController::make([
         'type' => 'anchore',
-        'routeName' => $routeName. 'index',
+        'routeName' => $routeName . 'index',
         'label' => 'Return',
         'icone' => 'fa-solid fa-arrow-left',
       ]),
     ];
   }
 
-  public static function editFields(): array {
+  public static function editFields($data = null): array {
     return [
       TextInput::make('id')->label('Id')->numeric()->readonly(),
       TextInput::make('phone')->tel()->label('Phone')->required(),
       TextInput::make('email')->email()->label('Email')->required(),
       TextInput::make('password')->password()->label('Password')->required(),
       Checkbox::make('is_active ')->label('Is Active'),
+      Select::make('role')->label('Role')->searchable()->multiple()->required()
+        ->options(Role::pluck('name', 'id')->toArray()),
+      ...match ($data?->roles->first()?->details_table) {
+        'system_user_details' => self::systemUserFields(),
+        'company_details' => self::companyFields(),
+        'user_details' => self::regularUserFields(),
+        default => [],
+      },
       DateTimePicker::make('created_at')->label('Created At')->readonly(),
       DateTimePicker::make('updated_at')->label('Updated At')->readonly(),
+    ];
+  }
+
+  protected static function systemUserFields(): array {
+    return [
+      TextInput::make('first_name')->label('First Name')->required(),
+      TextInput::make('sur_name')->label('Sur Name')->required(),
+      TextInput::make('pesel')->label('Pesel')->required(),
+    ];
+  }
+
+  protected static function companyFields(): array {
+    return [
+      TextInput::make('company_name')->label('Company Name')->required(),
+      TextInput::make('street')->label('Street')->required(),
+      TextInput::make('city')->label('City')->required(),
+      TextInput::make('postcode')->label('Postcode')->required(),
+      TextInput::make('nip')->label('Nip')->required(),
+      TextInput::make('regon')->label('Regon')->required(),
+      TextInput::make('krs')->label('Krs')->required(),
+    ];
+  }
+
+  protected static function regularUserFields(): array {
+    return [
+      TextInput::make('first_name')->label('First Name')->required(),
+      TextInput::make('sur_name')->label('Sur Name')->required(),
+      TextInput::make('pesel')->label('Pesel')->required(),
+      TextInput::make('street')->label('Street')->required(),
+      TextInput::make('city')->label('City')->required(),
+      TextInput::make('postcode')->label('Postcode')->required(),
     ];
   }
 
