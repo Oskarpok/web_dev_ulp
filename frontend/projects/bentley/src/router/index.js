ebc3f597b5dc97ad4be522a9a1b8dbca38226e54
OskarP <oskar43619@wp.pl>
2026-02-19 21:07:53 +0100

    Update index.js

--- frontend/projects/bentley/src/router/index.js
+++ frontend/projects/bentley/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,7 +18,7 @@ const router = createRouter({
     //   component: () => import('../views/AboutView.vue'),
     // },
 
-    
+
   ],
 })
 
