b5cacf26826e7215ba6afc2007151d90b1d4f232
OskarP <oskar43619@wp.pl>
2026-02-19 18:11:17 +0100

    Update UserResources.php

--- backend/packages/ulp/core/src/Http/Resources/Core/Users/UserResources.php
+++ backend/packages/ulp/core/src/Http/Resources/Core/Users/UserResources.php
@@ -17,17 +17,11 @@ class UserResources extends \Ulp\Core\Crud\Resources\BaseResource {
       TextInput::make('phone')->tel()->label('Phone')->required(),
       TextInput::make('email')->email()->label('Email')->required(),
       TextInput::make('password')->password()->label('Password')->required(),
-      Checkbox::make('is_active ')->label('Is Active'),
+      Checkbox::make('is_active')->label('Is Active'),
       Select::make('role')->label('Role')->searchable()->multiple()->required()
         ->options(Role::pluck('name', 'id')->toArray()),
-        ...array_map(fn($field) => $field->readonly(), 
-        match($data?->roles->first()?->details_table) {
-          'system_user_details' => self::systemUserFields(),
-          'company_details' => self::companyFields(),
-          'user_details' => self::regularUserFields(),
-          default => [],
-        }
-      ),
+
+        
     ];
   }
 
@@ -37,17 +31,12 @@ class UserResources extends \Ulp\Core\Crud\Resources\BaseResource {
       TextInput::make('phone')->tel()->label('Phone')->required()->readonly(),
       TextInput::make('email')->email()->label('Email')->required()->readonly(),
       TextInput::make('password')->password()->label('Password')->required()->readonly(),
-      Checkbox::make('is_active ')->label('Is Active')->disabled(),
+      Checkbox::make('is_active')->label('Is Active')->disabled(),
       Select::make('role')->label('Role')->searchable()->multiple()->required()
         ->options(Role::pluck('name', 'id')->toArray())->disabled(),
-      ...array_map(fn($field) => $field->readonly(), 
-        match($data?->roles->first()?->details_table) {
-          'system_user_details' => self::systemUserFields(),
-          'company_details' => self::companyFields(),
-          'user_details' => self::regularUserFields(),
-          default => [],
-        }
-      ),
+
+
+
       DateTimePicker::make('created_at')->label('Created At')->readonly(),
       DateTimePicker::make('updated_at')->label('Updated At')->readonly(),
     ];
@@ -59,48 +48,41 @@ class UserResources extends \Ulp\Core\Crud\Resources\BaseResource {
       TextInput::make('phone')->tel()->label('Phone')->required(),
       TextInput::make('email')->email()->label('Email')->required(),
       TextInput::make('password')->password()->label('Password')->required(),
-      Checkbox::make('is_active ')->label('Is Active'),
+      Checkbox::make('is_active')->label('Is Active'),
       Select::make('role')->label('Role')->searchable()->multiple()->required()
         ->options(Role::pluck('name', 'id')->toArray()),
-      ...match($data?->roles->first()?->details_table) {
-        'system_user_details' => self::systemUserFields(),
-        'company_details' => self::companyFields(),
-        'user_details' => self::regularUserFields(),
-        default => [],
-      },
+
+      TextInput::make('first_name')->label('First Name')->required()
+        ->visible(fn ($get) => !empty(array_intersect(['1', '3'], 
+          (array)($get('role') ?? [])))), 
+      TextInput::make('sur_name')->label('Sur Name')->required()
+        ->visible(fn ($get) => !empty(array_intersect(['1', '3'], 
+          (array)($get('role') ?? [])))),
+      TextInput::make('company_name')->label('Company Name')->required()
+        ->visible(fn ($get) => in_array('2', ($get('role')))),  
+
+      TextInput::make('street')->label('Street')->required()
+        ->visible(fn ($get) => !empty(array_intersect(['1', '2'], 
+          (array)($get('role') ?? [])))),
+      TextInput::make('city')->label('City')->required()
+        ->visible(fn ($get) => !empty(array_intersect(['1', '2'], 
+          (array)($get('role') ?? [])))),
+      TextInput::make('postcode')->label('Postcode')->required()
+        ->visible(fn ($get) => !empty(array_intersect(['1', '2'], 
+          (array)($get('role') ?? [])))),
+
+      TextInput::make('pesel')->label('Pesel')->required()
+        ->visible(fn ($get) => !empty(array_intersect(['1', '3'], 
+          (array)($get('role') ?? [])))),
+      TextInput::make('nip')->label('Nip')->required()    
+        ->visible(fn ($get) => in_array('2', ($get('role')))),  
+      TextInput::make('regon')->label('Regon')->required()        
+        ->visible(fn ($get) => in_array('2', ($get('role')))),  
+      TextInput::make('krs')->label('Krs')->required()        
+        ->visible(fn ($get) => in_array('2', ($get('role')))),  
+
       DateTimePicker::make('created_at')->label('Created At')->readonly(),
       DateTimePicker::make('updated_at')->label('Updated At')->readonly(),
-    ];
-  }
-
-  protected static function systemUserFields(): array {
-    return [
-      TextInput::make('first_name')->label('First Name')->required(),
-      TextInput::make('sur_name')->label('Sur Name')->required(),
-      TextInput::make('pesel')->label('Pesel')->required(),
-    ];
-  }
-
-  protected static function companyFields(): array {
-    return [
-      TextInput::make('company_name')->label('Company Name')->required(),
-      TextInput::make('street')->label('Street')->required(),
-      TextInput::make('city')->label('City')->required(),
-      TextInput::make('postcode')->label('Postcode')->required(),
-      TextInput::make('nip')->label('Nip')->required(),
-      TextInput::make('regon')->label('Regon')->required(),
-      TextInput::make('krs')->label('Krs')->required(),
-    ];
-  }
-
-  protected static function regularUserFields(): array {
-    return [
-      TextInput::make('first_name')->label('First Name')->required(),
-      TextInput::make('sur_name')->label('Sur Name')->required(),
-      TextInput::make('pesel')->label('Pesel')->required(),
-      TextInput::make('street')->label('Street')->required(),
-      TextInput::make('city')->label('City')->required(),
-      TextInput::make('postcode')->label('Postcode')->required(),
     ];
   }
 
