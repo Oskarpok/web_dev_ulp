1c847b6c59145b79f5953362dc36db8ff7c5b15c
OskarP <oskar43619@wp.pl>
2026-02-22 20:14:27 +0100

    Create TextTranslation.php

---
+++ backend/packages/ulp/core/src/Models/Core/Front/TextTranslation.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ulp\Core\Models\Core\Front;
+
+class TextTranslation extends \Ulp\Core\Models\Base {
+
+  protected $fillable = ['translation', 'language_id', 'text_id'];
+
+  public static function validationRules($id = null): array {
+    return [
+      'translation' => ['required', 'string', 'max:255', ],
+    ];
+  }
+
+}
