27607b91c4a1f53b3b7244a94bd7ff0462284f54
OskarP <oskar43619@wp.pl>
2026-02-23 20:57:10 +0100

    Update TextController.php

--- backend/packages/ulp/core/src/Http/Controllers/Api/TextController.php
+++ backend/packages/ulp/core/src/Http/Controllers/Api/TextController.php
@@ -4,14 +4,11 @@ declare(strict_types=1);
 
 namespace Ulp\Core\Http\Controllers\Api;
 
-use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\JsonResource;
-
 class TextController extends \Ulp\Core\Crud\Controller\BaseApiController {
 
   protected const MODEL_CLASS = \Ulp\Core\Models\Core\Front\Text::class;
   
-  public function getTranslations(Request $request) {
+  public function getTranslations() {
     return response()->json([
       'status' => 'success',
       'data' => self::MODEL_CLASS::with('translations.language')->get()->mapWithKeys(function ($text) {
