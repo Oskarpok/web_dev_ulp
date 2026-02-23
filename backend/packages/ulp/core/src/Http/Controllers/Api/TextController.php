02b0142529a1c0f2b4861b9e0a616df170194c2c
OskarP <oskar43619@wp.pl>
2026-02-23 22:13:47 +0100

    Update TextController.php

--- backend/packages/ulp/core/src/Http/Controllers/Api/TextController.php
+++ backend/packages/ulp/core/src/Http/Controllers/Api/TextController.php
@@ -9,16 +9,20 @@ class TextController extends \Ulp\Core\Crud\Controller\BaseApiController {
   protected const MODEL_CLASS = \Ulp\Core\Models\Core\Front\Text::class;
   
   public function getTranslations() {
-    return response()->json([
-      'status' => 'success',
-      'data' => self::MODEL_CLASS::with('translations.language')->get()->mapWithKeys(function ($text) {
+    $formatted = cache()->rememberForever('api_texts_dictionary', function () {
+      return self::MODEL_CLASS::with('translations.language')->get()->mapWithKeys(function ($text) {
         return [
           $text->name => $text->translations->mapWithKeys(function ($translation) {
             $shortcut = $translation->language->shortcut ?? 'unknown';
             return [$shortcut => $translation->translation];
           })
         ];
-      })
+      });
+    });
+
+    return response()->json([
+      'status' => 'success',
+      'data'   => $formatted
     ]);
   }
 
